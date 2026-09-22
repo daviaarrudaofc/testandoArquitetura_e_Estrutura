24ffbda304e3c76ec206dfa29b6dbe910fd6845a
davi arruda <daviarruda092@gmail.com>
2026-09-22 11:03:21 -0300

    utilizando @Qualifier

--- src/main/java/io/github/daviaarrudaofc/arquiteturaSpring/montadora/api/TesteFabricaController.java
+++ src/main/java/io/github/daviaarrudaofc/arquiteturaSpring/montadora/api/TesteFabricaController.java
@@ -2,6 +2,7 @@ package io.github.daviaarrudaofc.arquiteturaSpring.montadora.api;
 
 import io.github.daviaarrudaofc.arquiteturaSpring.montadora.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/carros")
 public class TesteFabricaController {
 
-    @Autowired
+    @Autowired//->pede um objeto ao Spring
+    // Injeta nesta variável um objeto Motor que já foi criado e gerenciado pelo Spring.
+
+    @Qualifier("motorTurbo")//-> especifica qual objeto usar quando existem vários Beans do mesmo tipo
+    // Especifica qual Bean do tipo Motor o Spring deve injetar,
+    // pois existem vários Beans do mesmo tipo.
     private Motor motor;
 
     @PostMapping
